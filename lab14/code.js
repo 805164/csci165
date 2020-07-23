853024e3e2612039e6b13877e41a23922898d728
Sabina Mukhitova <66041024+805164@users.noreply.github.com>
2020-07-22 17:00:58 -0700

    UPDATE

--- lab14/code.js
+++ lab14/code.js
@@ -1,35 +1,46 @@
 add = function() {
     result = parseInt(jQuery('#operand_1').html()) + parseInt(jQuery('#operand_2').html());
-    jQuery('#result').html(result);
+    text = jQuery('#operand_1').html() +" + "+ jQuery('#operand_2').html() + "= ";
+    jQuery('#result').html(text + result);
     }
+    
     setup = function() {
     jQuery('#add_button').click(add)
     }
     jQuery(document).ready(setup)
 
-    minus = function() {
-        result = parseInt(jQuery('#operand_1').html()) - parseInt(jQuery('#operand_2').html());
-        jQuery('#result').html(result);
-        }
-        setup = function() {
-        jQuery('#minus_button').click(minus)
-        }
-        jQuery(document).ready(setup)    
 
-        multiply = function() {
-            result = parseInt(jQuery('#operand_1').html()) * parseInt(jQuery('#operand_2').html());
-            jQuery('#result').html(result);
-            }
-            setup = function() {
-            jQuery('#multiply_button').click(multiply)
-            }
-            jQuery(document).ready(setup)
+minus = function() {
+    result = parseInt(jQuery('#operand_1').html()) - parseInt(jQuery('#operand_2').html());
+    text = jQuery('#operand_1').html() +" - "+ jQuery('#operand_2').html() + "= ";
+    jQuery('#result').html(text+result);
+    }
 
-            divide = function() {
-                result = parseInt(jQuery('#operand_1').html()) / parseInt(jQuery('#operand_2').html());
-                jQuery('#result').html(result);
-                }
-                setup = function() {
-                jQuery('#divide_button').click(divide)
-                }
-                jQuery(document).ready(setup)
+    setup = function() {
+    jQuery('#minus_button').click(minus)
+    }
+    jQuery(document).ready(setup)  
+
+
+multiply = function() {
+    result = parseInt(jQuery('#operand_1').html()) * parseInt(jQuery('#operand_2').html());
+    text = jQuery('#operand_1').html() +" * "+ jQuery('#operand_2').html() + "= ";
+    jQuery('#result').html(text + result);
+    }
+
+    setup = function() {
+    jQuery('#multiply_button').click(multiply)
+    }
+    jQuery(document).ready(setup)
+
+
+divide = function() {
+    result = parseInt(jQuery('#operand_1').html()) / parseInt(jQuery('#operand_2').html());
+    text = jQuery('#operand_1').html() +" / "+ jQuery('#operand_2').html() + "= ";
+    jQuery('#result').html(text + result); 
+    }
+
+    setup = function() {
+    jQuery('#divide_button').click(divide)
+    }
+    jQuery(document).ready(setup)
